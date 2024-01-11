0c3a8ebe27b61badd1475377b6f302c1c7acfeb8
VivekSingh <viveksingh0756@gmail.com>
2024-01-11 11:16:32 +0530

    Added files

---
+++ src/main/java/com/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.hello;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Vivek Singh");
+		System.out.println(StringUtils.capitalize("vivek singh"));
+	}
+}
